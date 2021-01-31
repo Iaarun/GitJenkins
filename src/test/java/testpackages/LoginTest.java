a680105719b79608c7fe04920162611cabd5e3ef
iaarun <automationbyaarun@gmail.com>
2021-01-31 14:43:01 +0530

    tag added

--- src/test/java/testpackages/LoginTest.java
+++ src/test/java/testpackages/LoginTest.java
@@ -1,6 +1,7 @@
 package testpackages;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,7 +23,7 @@ public class LoginTest {
 		System.out.println("Logo test ");
 		Assert.assertTrue(true);
 	}
-	
+	@AfterMethod
 	public void tearDown() {
 		System.out.println("Closing browser");
 	}
