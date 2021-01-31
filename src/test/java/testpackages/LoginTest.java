be07bc290523900bc6b0f9d241b163527fc8c680
iaarun <automationbyaarun@gmail.com>
2021-01-31 15:24:08 +0530

    resolving conflict

--- src/test/java/testpackages/LoginTest.java
+++ src/test/java/testpackages/LoginTest.java
@@ -6,34 +6,31 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTest {
-  
+
 	@BeforeMethod
-	public  void browserSetup() {
+	public void browserSetup() {
 		System.out.println("Launching browser");
 	}
-	
+
 	@Test
 	public void logintest() {
 		System.out.println("Login test ");
 		Assert.assertTrue(true);
 	}
-	
+
 	@Test
 	public void logoTest() {
 		System.out.println("Logo test ");
 		Assert.assertTrue(true);
 	}
-<<<<<<< HEAD
-	@AfterMethod
-=======
-	
+
 	@Test
 	public void titleTest() {
 		System.out.println("Title test ");
 		Assert.assertTrue(true);
 	}
-	
->>>>>>> 0532ac6865f12808433d267a5209b5494f7404c7
+
+	@AfterMethod
 	public void tearDown() {
 		System.out.println("Closing browser");
 	}
